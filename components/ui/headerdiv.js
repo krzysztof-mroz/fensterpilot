32f32816d33f9db2fc77e32cd410351e012901ed
Krzysztof Mróz <krzysztof.mroz@me.com>
2025-01-17 11:16:15 +0100

    aluplast salamander movie

--- components/ui/headerdiv.js
+++ components/ui/headerdiv.js
@@ -87,7 +87,7 @@ function Header(props) {
       {/* IKONKI I TYTUL STRONY */}
       <div className="w-100 w-50-l fl tc mv1 mh1 ">
      
-     {/* REKLAMY W SLAJDACH: */}
+     {/* {/* REKLAMY W SLAJDACH: */}
         <div className="dn db-l hover-w3-border-orange relative">
           <img 
             className="mr1 grow shadow-hover" 
@@ -108,11 +108,11 @@ function Header(props) {
           />
           <button onClick={handlePrevSlide} className="arrow left-arrow">&#10094;</button>
           <button onClick={handleNextSlide} className="arrow right-arrow">&#10095;</button>
-        </div> 
+        </div>  */}
 
-        {/* <div className={ifAnfrage === 'yes' ? 'dn db-l relative' : ''}>
+         <div className={ifAnfrage === 'yes' ? 'dn db-l relative' : ''}>
         <video
-        src="./movies/living.mp4" 
+        src="./movies/aluplast_salamander.mp4" 
         autoPlay 
         loop 
         muted
@@ -120,7 +120,7 @@ function Header(props) {
         controls={false} 
         style={{ width: "100%", height: "auto" }}
         type="video/mp4"/>
-        </div> */}
+        </div> 
            
 
         <h1 className="fl f2 ma1 mt3-l w-100 tc">{title}</h1>
