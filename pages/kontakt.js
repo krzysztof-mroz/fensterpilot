ce9ecc493de608c69765ff212ebd5877870a38fb
Krzysztof Mróz <krzysztof.mroz@me.com>
2025-01-20 14:11:46 +0100

    kontakt

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -17,13 +17,12 @@ function Kontakt() {
       <div className="flex flex-wrap justify-around mv1 tc fl w-100 ba b--moon-gray pa4">
         <Schnellkontakt />
         <div className="w-100 w-30-l ba b--moon-gray ma2 pa3 center"  >
-        <p className="gray f6">Kugemo UG (haftungsbeschränkt) </p>
+        <p className="gray f6">Fensterpilot.de </p>
         <p className="gray f6">Münzstraße 2 </p>
         <p className="gray f6">38100 Braunschweig </p>  
-        <p className="gray f6">VAT ID: DE345506278</p>
         <p className="gray f6">Tel: 0800 4470099</p>
-        <p className="gray f6">Email: info@polnische-fenster.com</p>
-        <p className="gray f6">Handelsregisternummer: HRB 209415</p>
+        <p className="gray f6">Email: info@fensterpilot.de</p>
+        
       </div>
       </div>
       
